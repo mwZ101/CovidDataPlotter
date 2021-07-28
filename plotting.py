3a5aca0bad646855d19a1af1751819624082e4a9
mwZ101 <marvinwu3@gmail.com>
2021-07-28 14:51:54 -0700

    Made the runPlots() function and the entire module less repetitive and more shorter by using a created function for making the figure with plots

--- plotting.py
+++ plotting.py
@@ -14,7 +14,7 @@ This function runs the dataset using the inputted province/territory string and 
 
 provTerrName: this argument is the string for the province/territory name, and is not null
 
-return: the function returns a list of the figures with plots 
+returns: the function returns a list of the figures with plots 
 """
 def runPlots(provTerrName):
     provinces = np.array(["Alberta", "British Columbia", "Manitoba", "New Brunswick",
@@ -65,181 +65,70 @@ def runPlots(provTerrName):
 
     print("Creating plots...\n")
 
-    """
-    Plot 1: Total number of cases
-    """
-    x1 = df_selected_total["date"].dt.date
-    y1 = df_selected_total["Total_Cases"]
 
-    fig1 = Figure(figsize=(10, 9))
-    ax1 = fig1.add_subplot()
-    fig1.subplots_adjust(bottom=0.18, top=0.93)
+    # Making all the relevant plots by passing the needed info into a function, and then adding into a list of figures
 
-    ax1.plot(x1, y1)
-    ax1.set_title("Total Number of COVID-19 Cases in " + provinces[selected_index] + " (Confirmed and Possible Cases)")
+    totalCasesFigure = _createPlot(df_selected_total, "Total_Cases", "Number of Cases",
+               "Total Number of COVID-19 Cases in " + provinces[selected_index] + " (Confirmed and Possible Cases)")
+    listOfFigures.append(totalCasesFigure)
 
-    ax1.set_xlabel("Date", labelpad=15)
-    ax1.set_ylabel("Number of Cases", labelpad=15)
+    confirmedCasesFigure = _createPlot(df_selected_confirmed, "Confirmed_Cases", "Number of Confirmed Cases",
+                                  "Total Number of Confirmed COVID-19 Cases in " + provinces[selected_index])
+    listOfFigures.append(confirmedCasesFigure)
 
-    monthInterval = mdates.MonthLocator(interval=1)
-    ax1.xaxis.set_major_locator(monthInterval)
-    ax1.tick_params(axis="x", labelrotation=45)
+    newCasesFigure = _createPlot(df_selected_newcases, "New_Cases", "Number of Newly Confirmed Cases",
+                                  "Number of Newly Confirmed COVID-19 Cases in " + provinces[selected_index])
+    listOfFigures.append(newCasesFigure)
 
-    yticks1 = mticker.MaxNLocator(15)
-    ax1.yaxis.set_major_locator(yticks1)
+    activeCasesFigure = _createPlot(df_selected_active, "Active_Cases", "Number of Active Cases",
+                                  "Number of Currently Active COVID-19 Cases in " + provinces[selected_index])
+    listOfFigures.append(activeCasesFigure)
 
-    listOfFigures.append(fig1)
+    numTestedFigure = _createPlot(df_selected_tested, "Tested", "Number of Tested Individuals",
+                                  "Number of Tested Individuals for COVID-19 in " + provinces[selected_index])
+    listOfFigures.append(numTestedFigure)
 
+    recoveredCasesFigure = _createPlot(df_selected_recovered, "Recovered_Cases", "Number of Recovered Cases",
+                                  "Number of Recovered COVID-19 Cases in " + provinces[selected_index])
+    listOfFigures.append(recoveredCasesFigure)
 
-    """
-    Plot 2: Number of Confirmed Cases per day
-    """
-    x2 = df_selected_confirmed["date"].dt.date
-    y2 = df_selected_confirmed["Confirmed_Cases"]
-
-    fig2 = Figure(figsize=(10, 9))
-    ax2 = fig2.add_subplot()
-    fig2.subplots_adjust(bottom=0.18, top=0.93)
-
-    ax2.plot(x2, y2)
-    ax2.set_title("Total Number of Confirmed COVID-19 Cases in " + provinces[selected_index])
-
-    ax2.set_xlabel("Date", labelpad=15)
-    ax2.set_ylabel("Number of Confirmed Cases", labelpad=15)
-
-    ax2.xaxis.set_major_locator(monthInterval)
-    ax2.tick_params(axis="x", labelrotation=45)
-
-    yticks2 = mticker.MaxNLocator(15)
-    ax2.yaxis.set_major_locator(yticks2)
-
-    listOfFigures.append(fig2)
-
-
-    """
-    Plot 3: Number of Newly Confirmed Cases
-    """
-    x3 = df_selected_newcases["date"].dt.date
-    y3 = df_selected_newcases["New_Cases"]
-
-    fig3 = Figure(figsize=(10, 9))
-    ax3 = fig3.add_subplot()
-    fig3.subplots_adjust(bottom=0.18, top=0.93)
-
-    ax3.plot(x3, y3)
-    ax3.set_title("Number of Newly Confirmed COVID-19 Cases in " + provinces[selected_index])
-
-    ax3.set_xlabel("Date", labelpad=15)
-    ax3.set_ylabel("Number of Confirmed Cases", labelpad=15)
-
-    ax3.xaxis.set_major_locator(monthInterval)
-    ax3.tick_params(axis="x", labelrotation=45)
-
-    yticks3 = mticker.MaxNLocator(15)
-    ax3.yaxis.set_major_locator(yticks3)
-
-    listOfFigures.append(fig3)
-
-
-    """
-    Plot 4: Number of Currently Active Cases
-    """
-    x4 = df_selected_active["date"].dt.date
-    y4 = df_selected_active["Active_Cases"]
-
-    fig4 = Figure(figsize=(10, 9))
-    ax4 = fig4.add_subplot()
-    fig4.subplots_adjust(bottom=0.18, top=0.93)
-
-    ax4.plot(x4, y4)
-    ax4.set_title("Number of Currently Active COVID-19 Cases in " + provinces[selected_index])
-
-    ax4.set_xlabel("Date", labelpad=15)
-    ax4.set_ylabel("Number of Active Cases", labelpad=15)
-
-    ax4.xaxis.set_major_locator(monthInterval)
-    ax4.tick_params(axis="x", labelrotation=45)
-
-    yticks4 = mticker.MaxNLocator(15)
-    ax4.yaxis.set_major_locator(yticks4)
-
-    listOfFigures.append(fig4)
-
-
-    """
-    Plot 5: Number of Tested Individuals
-    """
-    x5 = df_selected_tested["date"].dt.date
-    y5 = df_selected_tested["Tested"]
-
-    fig5 = Figure(figsize=(10, 9))
-    ax5 = fig5.add_subplot()
-    fig5.subplots_adjust(bottom=0.18, top=0.93)
-
-    ax5.plot(x5, y5)
-    ax5.set_title("Number of Tested Individuals for COVID-19 in " + provinces[selected_index])
-
-    ax5.set_xlabel("Date", labelpad=15)
-    ax5.set_ylabel("Number of Tested Individuals", labelpad=15)
-
-    monthInterval5 = mdates.MonthLocator(interval=1)
-    ax5.xaxis.set_major_locator(monthInterval5)
-    ax5.tick_params(axis="x", labelrotation=45)
-
-    yticks5 = mticker.MaxNLocator(15)
-    ax5.yaxis.set_major_locator(yticks5)
-
-    listOfFigures.append(fig5)
-
-
-    """
-    Plot 6: Number of Recovered Cases
-    """
-    x6 = df_selected_recovered["date"].dt.date
-    y6 = df_selected_recovered["Recovered_Cases"]
-
-    fig6 = Figure(figsize=(10, 9))
-    ax6 = fig6.add_subplot()
-    fig6.subplots_adjust(bottom=0.18, top=0.93)
-
-    ax6.plot(x6, y6)
-    ax6.set_title("Number of Recovered COVID-19 Cases in " + provinces[selected_index])
-
-    ax6.set_xlabel("Date", labelpad=15)
-    ax6.set_ylabel("Number of Recovered Cases", labelpad=15)
-
-    ax6.xaxis.set_major_locator(monthInterval)
-    ax6.tick_params(axis="x", labelrotation=45)
-
-    yticks6 = mticker.MaxNLocator(15)
-    ax6.yaxis.set_major_locator(yticks6)
-
-    listOfFigures.append(fig6)
-
-
-    """
-    Plot 7: Number of Deaths
-    """
-    x7 = df_selected_deaths["date"].dt.date
-    y7 = df_selected_deaths["Deaths"]
-
-    fig7 = Figure(figsize=(10, 9))
-    ax7 = fig7.add_subplot()
-    fig7.subplots_adjust(bottom=0.18, top=0.93)
-
-    ax7.plot(x7, y7)
-    ax7.set_title("Number of Deaths from COVID-19 in " + provinces[selected_index])
-
-    ax7.set_xlabel("Date", labelpad=15)
-    ax7.set_ylabel("Number of Deaths", labelpad=15)
-
-    ax7.xaxis.set_major_locator(monthInterval)
-    ax7.tick_params(axis="x", labelrotation=45)
-
-    yticks7 = mticker.MaxNLocator(15)
-    ax7.yaxis.set_major_locator(yticks7)
-
-    listOfFigures.append(fig7)
-
+    numDeathsFigure = _createPlot(df_selected_deaths, "Deaths", "Number of Deaths",
+                                  "Number of Deaths from COVID-19 in " + provinces[selected_index])
+    listOfFigures.append(numDeathsFigure)
 
     return listOfFigures
+
+"""
+This function makes a figure with a plot as specified. 
+
+df_selected: this is a dataframe that is used for the x and y coordinates, and is not null
+dataSelectedColumn: this string is the column's name of the dataframe to be used for the y coordinates
+
+yaxisLabel: this string is for the y-axis label of the plot
+plotTitle: this string is the specified plot title
+
+returns: this function returns the created figure with the plot
+"""
+def _createPlot(df_selected, dataSelectedColumn, yaxisLabel, plotTitle):
+    x = df_selected["date"].dt.date
+    y = df_selected[dataSelectedColumn]
+
+    fig = Figure(figsize=(10, 9))
+    ax = fig.add_subplot()
+    fig.subplots_adjust(bottom=0.18, top=0.93)
+
+    ax.plot(x, y)
+    ax.set_title(plotTitle)
+
+    ax.set_xlabel("Date", labelpad=15)
+    ax.set_ylabel(yaxisLabel, labelpad=15)
+
+    monthInterval = mdates.MonthLocator(interval=1)
+    ax.xaxis.set_major_locator(monthInterval)
+    ax.tick_params(axis="x", labelrotation=45)
+
+    yticks = mticker.MaxNLocator(15)
+    ax.yaxis.set_major_locator(yticks)
+
+    return fig
+
